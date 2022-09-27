9ff690a441529d37339ad5c722dbdfcc033cef43
Ruslan <35464027+DENBINGON@users.noreply.github.com>
2022-09-28 02:35:36 +0300

    Add resources enum

---
+++ KubSTU-iOS/Enums.swift
@@ -0,0 +1,27 @@
+//
+//  Enums.swift
+//  KubSTU-iOS
+//
+//  Created by Руслан Соловьев on 23.09.2022.
+//
+
+import SwiftUI
+enum Resources {
+    
+    enum Images {
+        
+        static var news = Image(systemName: "newspaper.fill")
+        static var find = Image(systemName:"magnifyingglass")
+        static var schedule = Image(systemName:"calendar")
+        static var user = Image(systemName:"person.fill")
+        static var logo = Image("KubSTU-Logo-blue-trans-300x300")
+    }
+    
+    enum Colors {
+        static var accent = Color.accentColor
+    }
+    
+    enum Text{
+        
+    }
+}
